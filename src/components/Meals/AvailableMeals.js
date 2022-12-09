3d861e8ef6a645212ff02fc76200e518c86ab803
zooyaho <jiwoo1268@naver.com>
2022-12-09 16:30:01 +0900

    feat: connect meal component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,7 +30,15 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      itemPrice={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
       <Card>
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
-function MealItem({ name, description, itemPrice }) {
+function MealItem({ id, name, description, itemPrice }) {
   const price = `$${itemPrice.toFixed(2)}`;
   return (
     <li className={classes.meal}>
@@ -8,6 +9,9 @@ function MealItem({ name, description, itemPrice }) {
         <h3>{name}</h3>
         <div className={classes.description}>{description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={id} />
       </div>
     </li>
   );
