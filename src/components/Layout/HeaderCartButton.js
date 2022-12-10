6e485361cc5a82fc1812522f6d5b104a3cad17f0
zooyaho <jiwoo1268@naver.com>
2022-12-10 14:26:41 +0900

    feat: Add number of cart items from cart badge

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton({ onClick }) {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce(
+    (curNumber, item) => curNumber + item.amount,
+    0
+  ); // 장바구니 항목 수
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.CartIcon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
