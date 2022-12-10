8759767d0a7862208d5ab10ff22de4ecb277f4d0
zooyaho <jiwoo1268@naver.com>
2022-12-10 14:55:07 +0900

    feat: Connect cart context and reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,42 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0, // 총 결제 가격
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_CART") {
+    const updatedItems = state.items.concat(action.item); // 새로운 배열을 리턴!! push() X
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState; // action이 없을 경우 초기값 리턴~~
+};
 
 // provider의 역할은 cart contex를 관리하고, 이 context를 접근하는 모든 컴포넌트에 제공한다.
 function CartProvider({ children }) {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  // reducer를 사용하여 cart를 state로 관리한다.
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_CART", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_CART", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
