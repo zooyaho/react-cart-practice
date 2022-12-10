84c0eb7dae7a9b95d9186817c73e2dfe9ffb020f
zooyaho <jiwoo1268@naver.com>
2022-12-10 16:14:39 +0900

    feat: Delete dummy cart items data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 function Cart({ onClose }) {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = cartCtx.totalAmount.toFixed(2);
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -14,13 +22,13 @@ function Cart({ onClose }) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={onClose}>
           close
         </button>
-        <button className={classes.button}>order</button>
+        {hasItems && <button className={classes.button}>order</button>}
       </div>
     </Modal>
   );
